fb80cd1a3455c14fc33852928d696861fb9e57b4
mehedihasanuchee <mehedihasansuchee@outlook.com>
2025-10-19 08:03:18 +0600

    Two Sum

--- 0001_TowSum.py
+++ 0001_TowSum.py
@@ -2,13 +2,11 @@
 
 class Solution:
     def twoSum(self, nums, target):
-        seen = {}  # এখানে আমরা সংখ্যা আর তার index রাখব
+        seen = {}
         for i, num in enumerate(nums):
-            complement = target - num  # target এ পৌছাতে আর কত দরকার
+            complement = target - num 
             
             if complement in seen:
-                # যদি complement আগে দেখা থাকে, তাহলে উত্তর পাওয়া গেছে
                 return [seen[complement], i]
             
-            # না থাকলে, এই সংখ্যাটা map এ রাখি
             seen[num] = i
